179ab1fbd66a70dd12c381285237f2fdc178b1fc
Radita Liem <hesti@ut.ee>
2017-12-09 23:31:20 +0200

    Add satellite frequency length

--- app.py
+++ app.py
@@ -18,14 +18,26 @@ def index():
 	with open('static/json/train.json', 'r') as f:
 		train = json.load(f)
 		train_length = len(train)
+		train_band1_length = len(train[0]['band_1'])
+		train_band2_length = len(train[0]['band_1'])
 
 	with open('static/json/test.json', 'r') as f:
 		test = json.load(f)
 		test_length = len(test)
+		test_band1_length = len(test[0]['band_1'])
+		test_band2_length = len(test[0]['band_2'])
 
 	#print (data[0])
 
-	return render_template('index.html', train = train, test = test, train_length = train_length, test_length = test_length)
+	return render_template('index.html', 
+		train = train, 
+		test = test, 
+		train_length = train_length, 
+		test_length = test_length,
+		train_band1_length = train_band1_length,
+		train_band2_length = train_band2_length,
+		test_band1_length = test_band1_length,
+		test_band2_length = test_band2_length)
 
 @app.route('/data/<path:path>')
 def send_json(path):
